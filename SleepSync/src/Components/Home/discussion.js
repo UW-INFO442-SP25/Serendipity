642f5fe4f5c085b1b5a871ec8e7a99705a75eb6f
imangipudi <m1ra@uw.edu>
2025-04-28 15:49:03 -0700

    scaffolding changes

---
+++ SleepSync/src/Components/Home/discussion.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const discussion = () => {
+  return (
+    <div>discussion</div>
+  )
+}
+
+export default discussion
---
+++ SleepSync/src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyChlQIORpFKLlcRNVZIXLtF--Z1x4flZ2g",
+  authDomain: "serendipity-4a9ed.firebaseapp.com",
+  projectId: "serendipity-4a9ed",
+  storageBucket: "serendipity-4a9ed.firebasestorage.app",
+  messagingSenderId: "807197840316",
+  appId: "1:807197840316:web:de441d338d5406bc1191b9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
