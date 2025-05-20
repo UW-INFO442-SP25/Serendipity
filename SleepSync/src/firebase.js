38d27d43ac93b132ddd7224b20fd83154b7dc75a
Jiuzhen Han <jhan14@uw.edu>
2025-05-20 00:37:28 -0700

    Initial commit with Vite + React + Firebase integration

--- SleepSync/src/firebase.js
+++ SleepSync/src/firebase.js
@@ -3,11 +3,6 @@ import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-export default app;
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app"
 // Add Firestore support
 import { getFirestore } from "firebase/firestore"
 
@@ -16,17 +11,17 @@ const firebaseConfig = {
   apiKey: "AIzaSyChlQIORpFKLlcRNVZIXLtF--Z1x4flZ2g",
   authDomain: "serendipity-4a9ed.firebaseapp.com",
   projectId: "serendipity-4a9ed",
-  storageBucket: "serendipity-4a9ed.firebasestorage.app",
+  storageBucket: "serendipity-4a9ed.appspot.com",
   messagingSenderId: "807197840316",
   appId: "1:807197840316:web:de441d338d5406bc1191b9"
-}
+};
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig)
+const app = initializeApp(firebaseConfig);
 
 // Initialize Firestore
-const db = getFirestore(app)
+const db = getFirestore(app);
 
 // Export Firebase app and Firestore instance
-export default app
-export { db }
+export default app;
+export { db };
