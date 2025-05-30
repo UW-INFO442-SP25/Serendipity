d793083a60f1143a8327dbbb7b4e667bfe185e13
kellythai99 <kthai2@uw.edu>
2025-05-30 13:48:19 -0700

    Add mobile and tablet responsive styling for all pages + navbar hamburger menu

--- SleepSync/src/Components/Home/navbar.jsx
+++ SleepSync/src/Components/Home/navbar.jsx
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import './Navbar.css';
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => setMenuOpen((prev) => !prev);
+
   return (
     <header className="navbar">
       <div className="logo">
         <NavLink to="/home">SleepSync</NavLink>
       </div>
-      <nav>
-        <NavLink to="/home">Home</NavLink>
-        <NavLink to="/resources">Resources</NavLink>
-        <NavLink to="/discussion">Discussion Forum</NavLink>
-        <NavLink to="/tracker">Behavior Tracker</NavLink>
-        <NavLink to="/login" className="profile-icon">👤</NavLink>
+
+      {/* Hamburger icon (☰) */}
+      <div className="hamburger" onClick={toggleMenu}>
+        ☰
+      </div>
+
+      <nav className={menuOpen ? 'show-menu' : ''}>
+        <NavLink to="/home" onClick={() => setMenuOpen(false)}>Home</NavLink>
+        <NavLink to="/resources" onClick={() => setMenuOpen(false)}>Resources</NavLink>
+        <NavLink to="/discussion" onClick={() => setMenuOpen(false)}>Discussion Forum</NavLink>
+        <NavLink to="/tracker" onClick={() => setMenuOpen(false)}>Behavior Tracker</NavLink>
+        <NavLink to="/login" className="profile-icon" onClick={() => setMenuOpen(false)}>👤</NavLink>
       </nav>
     </header>
   );
